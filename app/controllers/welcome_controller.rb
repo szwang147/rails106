7ec3146b947403bce5e50f9d21856489fbe527db
simon <wangzd.fudan@gmail.com>
2017-01-10 01:43:00 +0800

    use simple_form

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -54,3 +54,51 @@ end
   end
 
 end
+
+
+class GroupsController < ApplicationController
+
+  def index
+    @groups = Group.all
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to groups_path
+    else
+      render :edit
+  end
+end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update
+      redirect_to groups_path notice: "update"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+      redirect_to groups_path, alert: "deleted"
+  end
+  private
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+end 6min50seconds
