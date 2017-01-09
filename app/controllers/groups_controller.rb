003c11d49ff82f2ac1d21d68b17c87f53cb36966
simon <wangzd.fudan@gmail.com>
2017-01-10 00:19:48 +0800

    implement group create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,23 @@
 class GroupsController < ApplicationController
-def index
-  @groups = Group.all
-end
+  def index
+    @groups = Group.all
+  end
 
-def new
-  @group = Group.new
-end
+  def new
+    @group = Group.new
+  end
+
+  def create
+  @group = Group.new(group_params)
+  @group.save
+  redirect_to groups_path
+  end
+
+  private
+  
+  def group_params
+    params.require(:group).permit(:title, :description)
+    # 其实上面 就是rails g model group title:string description:text
+  end
+
 end
