f8f259f7acc31d5a59aa49065a1a8d9ef606ff53
simon <wangzd.fudan@gmail.com>
2017-01-12 00:58:03 +0800

    implement posts pagination

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,8 +22,9 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
-    @posts = @group.posts.recent
+    @posts = @group.posts.recent.paginate(page: params[:page], per_page: 5)
     # @posts = @group.posts.order("created_at DESC")
+
 
   end
 
