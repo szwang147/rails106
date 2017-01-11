2b5982dd5c2817f4be45175c69a93cbec19caf65
simon <wangzd.fudan@gmail.com>
2017-01-12 03:06:59 +0800

    join and quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,8 +24,6 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
     @posts = @group.posts.recent.paginate(page: params[:page], per_page: 5)
     # @posts = @group.posts.order("created_at DESC")
-
-
   end
 
 
@@ -33,11 +31,11 @@ class GroupsController < ApplicationController
   end
 
   def update
-    if @group.update(group_params)
-    redirect_to groups_path, notice:"update success"
-  else
-    render :edit
-  end
+      if @group.update(group_params)
+      redirect_to groups_path, notice:"update success"
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -46,6 +44,34 @@ class GroupsController < ApplicationController
           alert:"group deleted"
     flash[:warning] = "deleted again"
 end
+
+def join
+  @group = Group.find(params[:id])
+
+  if !current_user.is_member_of?(@group)
+    current_user.join!(@group)
+      flash[:notice] = "加入本讨论版成功！"
+  else
+      flash[:warning] = "你已经是本讨论版成员了！"
+  end
+  redirect_to group_path(@group)
+end
+
+def quit
+  @group = Group.find(params[:id])
+
+  if current_user.is_member_of?(@group)
+    current_user.quit!(@group)
+      flash[:alert] = "已退出本讨论版！"
+    # notice:"quit the team success"
+  else
+      flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
+    # warning:"you are not the team member, how to quit"
+  end
+  redirect_to group_path(@group)
+end
+
+
 
   private
 def find_group_and_check_permission
