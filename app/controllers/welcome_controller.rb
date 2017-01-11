1e29aba5aa60ccc21206b085af4ef5e88c1afc35
simon <wangzd.fudan@gmail.com>
2017-01-11 22:28:41 +0800

    just type crud

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -34,7 +34,7 @@ end
 
 def update
   @group = Group.find(params[:id])
-  if @group.update(group_params)
+  if @group.save(group_params)
     redirect_to groups_path, notice:'update success'
   else
     render :edit
@@ -102,3 +102,50 @@ end
     params.require(:group).permit(:title, :description)
   end
 end 6min50seconds
+
+
+class GroupsController < ApplicationController
+  def index
+    @groups = Group.all
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to groups_path
+    else
+      render :edit
+  end
+
+  def update
+    @group = Group.find(parmas[:id])
+    if @group.save
+      redirect_to groups_path, notice:"updated"
+    else
+      render: edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(parmas[:id])
+    @group.destroy
+      redirect_to groups_path, warning:"deteled"
+    end
+
+  private
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+end 7min17seconds
