8c939e1d6968ce9011487503ec95cae7c596b7a3
simon <wangzd.fudan@gmail.com>
2017-01-12 04:31:19 +0800

    implement edit delete function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,44 @@
 class PostsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create]
 
-def new
-  @group = Group.find(params[:group_id])
-  @post = Post.new
-end
-
-def create
-  @group = Group.find(params[:group_id])
-  @post = Post.new(post_params)
-  @post.group = @group
-  @post.user = current_user
-
-  if @post.save
-    redirect_to group_path(@group)
-  else
-    render :new
+  def new
+    @group = Group.find(params[:group_id])
+    @post = Post.new
   end
-end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @post = Post.new(post_params)
+    @post.group = @group
+    @post.user = current_user
+
+    if @post.save
+      redirect_to group_path(@group)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "update success"
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path,  alert: "deleted"
+  end
 
 private
  def post_params
